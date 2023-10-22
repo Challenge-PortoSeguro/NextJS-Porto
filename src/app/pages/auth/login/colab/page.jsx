1a04662a21db4c1e3a862a7d6da36a425e98bf1f
VitorMiranda11 <vitormiranda4321@outlook.com>
2023-10-22 00:30:27 -0300

    estruturando envio de dados de login do colaborador para API

--- src/app/pages/auth/login/colab/page.jsx
+++ src/app/pages/auth/login/colab/page.jsx
@@ -1,11 +1,28 @@
+'use client';
 import ButtonPrimary from "@/components/Button/variants/primary";
 import ButtonLink from "@/components/Button/variants/link";
 import "../styles.css";
 import Input from "@/components/Input/page";
 import Image from "next/image";
 import Logo from "@/assets/images/logo.png";
+import { useState } from "react";
 
 export default function Login() {
+    const [form , setForm] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+        console.log(form);
+    }
+
+    const onSubmit = (e) => {
+        console.log(form);
+    }
+
     return (
         <main className="container-login">
             <div className="image-colab" />
@@ -17,13 +34,17 @@ export default function Login() {
                         label="E-mail"
                         type="email"
                         placeholder="Digite seu e-mail"
+                        name={"email"}
+                        onChange={handleChange}
                     />
                     <Input
                         label="Senha"
                         type="password"
                         placeholder="Digite sua senha"
+                        name={"password"}
+                        onChange={handleChange}
                     />
-                    <ButtonPrimary redirect="/pages/profile/colab">Entrar</ButtonPrimary>
+                    <ButtonPrimary onClick={onSubmit}>Entrar</ButtonPrimary>
                     <ButtonLink redirect="/pages/auth/register/colab">Cadastre sua conta</ButtonLink>
                     <ButtonLink redirect="/pages/auth/login/client">É um cliente?</ButtonLink>
                 </form>
