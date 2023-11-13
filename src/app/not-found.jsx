9be2f7c5a31d436c0007baa31e799e7639ca4ec4
PedroFigueiredo7 <pedh.foliveira@gmail.com>
2023-11-13 13:27:56 -0300

    ajustes na pagina de notfound

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,8 +3,9 @@ import Button from "@/components/Button/variants/primary"
 export default function NotFound(){
     return(
         <div style={({ display: "flex", flexDirection: "column", gap: "32px", justifyContent: "center", alignItems: "center", height: "calc(100vh - 96px)" })}>
+            <h1 style={({ fontSize: "48px" })}>OOPS</h1>
             <h1 style={({ fontSize: "48px" })}>404 - Página não encontrada</h1>
-            <Button redirect="/">Voltar para a página inicial</Button>
+            <Button redirect="/">Página inicial</Button>
         </div>
     )
 }
